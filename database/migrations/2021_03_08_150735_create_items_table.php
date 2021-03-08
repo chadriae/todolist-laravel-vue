520b112b5c0c25d4874f234e54914b3eede1f073
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-03-08 17:03:08 +0100

    First setup of app with basic working functions

---
+++ database/migrations/2021_03_08_150735_create_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('completed')->default(false);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
