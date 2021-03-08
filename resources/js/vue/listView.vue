7bb3954490a94d6445ade1fb9f36fc0e288530f9
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-03-08 21:01:30 +0100

    Working to app with Vue

---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div 
+        v-for="(item, index) in items"
+        :key='index'
+        >
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemchanged="$emit('reloadlist')" 
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import listItem from './listItem';
+import ListItem from './listItem.vue';
+
+export default {
+    props: ['items'],
+    components: {
+        listItem
+        
+    }
+}
+</script>
+
+<style scoped>
+    .item{
+       background: whitesmoke;
+       padding: 5px;
+       margin-top: 5px;
+    }
+</style>
